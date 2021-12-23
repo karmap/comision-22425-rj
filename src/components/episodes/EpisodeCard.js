69c8ac1dbc9cc5f8239e246def7b0be44bc7ef0c
Adrian Escalante <adresc@gmail.com>
2021-12-22 19:35:24 -0600

    getEpisodes con Promise y setTimeout

--- src/components/episodes/EpisodeCard.js
+++ src/components/episodes/EpisodeCard.js
@@ -1,10 +1,14 @@
 import React from 'react'
 
-const EpisodeCard = ( {episode} ) => {
+const EpisodeCard = ( {episode, add} ) => {
     return (
         <div>
             <h2>{episode.title}</h2>
             <strong>{episode.id}</strong>
+            <br></br>
+            <button onClick={ () => {add(episode.id)} }>
+                Agregar a watchlist
+            </button>
         </div>
     )
 }
--- src/components/episodes/EpisodesList.js
+++ src/components/episodes/EpisodesList.js
@@ -1,25 +1,42 @@
 import React from 'react'
 import EpisodeCard from './EpisodeCard'
+import { useEffect, useState } from 'react'
 
 const EpisodesList = () => {
 
-    const episodes = [
-        {
-            title: 'Pilot',
-            id: 'S01E01'
-        },
-        {
-            title: 'Segundo episodio',
-            id: 'S01E02'
-        }
-    ]
+    const [episodes, setEpisodes] = useState([])
+    const [watchlist, setWatchlist] = useState([])
+
+    useEffect( ()=>{
+        getEpisodes()
+    }, [])
+
+    const getEpisodes = () => {
+        const getPromise = new Promise( (res, rej) => {
+            const episodes = [
+                {title:'ep 1', id:'s01'},
+                {title:'ep 2', id:'s02'},
+                {title:'ep 3', id:'s03'}
+            ]
+            setTimeout(() => {
+                res(episodes)
+            }, 5000);
+        })
+        getPromise.then( res => setEpisodes(res))
+    }
+
+    const addToWatchlist = id => {
+        setWatchlist( [...watchlist, id] )
+    }
 
     return (
         <div>
             <h1>Lista de Episodios</h1>
             {episodes.map( e => 
-                <EpisodeCard episode={e}/>
+                <EpisodeCard episode={e} add={addToWatchlist}/>
             )}
+            <h1>Lista por ver</h1>
+            {watchlist.map( e => <div>{e}</div> )}
         </div>
     )
 }
